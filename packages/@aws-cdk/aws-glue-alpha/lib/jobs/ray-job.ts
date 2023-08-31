6b80ef201a7c34adb95c571ba2ab5aa9b3bb7eba
Askar Serikov <askar.k.serikov@gmail.com>
2023-08-31 15:35:25 +0000

    ray job definition and most props covered

--- packages/@aws-cdk/aws-glue-alpha/lib/jobs/ray-job.ts
+++ packages/@aws-cdk/aws-glue-alpha/lib/jobs/ray-job.ts
@@ -3,20 +3,27 @@ import { Bucket } from 'aws-cdk-lib/aws-s3';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { Job, JobProperties } from './job';
 import { Construct } from 'constructs';
-import { JobType, GlueVersion, JobLanguage, WorkerType } from '../constants';
+import { JobType, GlueVersion, JobLanguage, WorkerType, Runtime } from '../constants';
 
 /**
  * Ray Jobs class
  *
  * Glue ray only supports worker type Z.2X and Glue version 4.0.
- * Runtime will default to Ray2.3 and min workers will default to 3.
+ * Runtime will default to Ray2.4 and min workers will default to 3.
  *
  */
 
 /**
  * Properties for creating a Ray Glue job
  */
-export interface RayJobProperties extends JobProperties {}
+export interface RayJobProperties extends JobProperties {
+  /**
+   * Sets the Ray runtime environment version
+   *
+   * @default - Runtime version will default to Ray2.4
+   */
+  readonly runtime?: Runtime
+}
 
 /**
  * A Ray Glue Job
@@ -42,6 +49,11 @@ export class RayJob extends Job {
       physicalName: props.jobName,
     });
 
+    // List of supported Glue versions by Ray
+    const supportedGlueVersions = [
+      GlueVersion.V4_0,            
+    ]
+
     // Set up role and permissions for principal
     this.role = props.role, {
       assumedBy: new iam.ServicePrincipal('glue.amazonaws.com'),
@@ -49,16 +61,41 @@ export class RayJob extends Job {
     };
     this.grantPrincipal = this.role;
 
-    // Conbine command line arguments into a single line item
+    // Combine command line arguments into a single line item
     const defaultArguments = {
       ...this.checkNoReservedArgs(props.defaultArguments),
     };
 
-    
+    if (props.workerType && props.workerType !== WorkerType.Z_2X) {
+      throw new Error('Ray jobs only support Z.2X worker type');
+    };
+
+    if (props.glueVersion && !(supportedGlueVersions.includes(props.glueVersion))) {
+      throw new Error('You must set GlueVersion to 4.0 or greater');
+    };
+
+    const jobResource = new CfnJob(this, 'Resource', {
+      name: props.jobName,
+      description: props.description,
+      role: this.role.roleArn,
+      command: {
+        name: JobType.RAY,
+        scriptLocation: this.codeS3ObjectUrl(props.script),
+        runtime: props.runtime ? props.runtime : Runtime.RAY_TWO_FOUR,
+      },
+      glueVersion: props.glueVersion ? props.glueVersion : GlueVersion.V4_0,
+      workerType: props.workerType ? props.workerType : WorkerType.Z_2X,
+      numberOfWorkers: props.numberOrWorkers,
+      maxRetries: props.maxRetries,
+      executionProperty: props.maxConcurrentRuns ? { maxConcurrentRuns: props.maxConcurrentRuns } : undefined,
+      //notificationProperty: props.notifyDelayAfter ? { notifyDelayAfter: props.notifyDelayAfter.toMinutes() } : undefined,
+      timeout: props.timeout?.toMinutes(),
+      connections: props.connections ? { connections: props.connections.map((connection) => connection.connectionName) } : undefined,
+      securityConfiguration: props.securityConfiguration?.securityConfigurationName,
+      tags: props.tags,
+      defaultArguments,
+    });
 
   }
-
-
-
 
 }
